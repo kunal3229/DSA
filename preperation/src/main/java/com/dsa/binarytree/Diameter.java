dad208da5dcba0bdbfd7afcbe7b3ac7b9e367125
kunal <kunalverma3229@gmail.com>
2025-05-09 08:27:42 +0530

    tree

---
+++ preperation/src/main/java/com/dsa/binarytree/Diameter.java
@@ -0,0 +1,22 @@
+package com.dsa.binarytree;
+
+public class Diameter {
+
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root){
+        height(root);
+        return diameter;
+    }
+    int height(TreeNode node){
+        if(node == null) return 0;
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        int die = leftHeight + rightHeight + 1;
+        diameter = Math.max(diameter, die);
+
+        return Math.max(leftHeight, rightHeight)+1;
+    }
+
+}
